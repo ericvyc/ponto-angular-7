0181d648bb1d935243a67f792b5fd078014c2558
ericvyc <eric_vilar2007@hotmail.com>
2019-01-25 12:59:23 -0500

    Login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatButtonModule } from '@angular/material';
+
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
+import { LoginModule, LoginRoutingModule } from './autenticacao';
 
 @NgModule({
   declarations: [
@@ -10,6 +16,12 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    LoginModule,
+    LoginRoutingModule,
+
+    //Modulo raiz deve ser o ultimo
     AppRoutingModule
   ],
   providers: [],
