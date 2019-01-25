f49686418daf68c3d9dc43fdb8841d62b874f04d
ericvyc <eric_vilar2007@hotmail.com>
2019-01-25 14:51:45 -0500

    Modelo de login

--- src/app/autenticacao/login/components/login/login.component.ts
+++ src/app/autenticacao/login/components/login/login.component.ts
@@ -3,6 +3,8 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { MatSnackBar } from '@angular/material';
 
+import { Login } from "../../models";
+
 @Component({
   selector: 'app-login-pf',
   templateUrl: './login.component.html',
@@ -34,6 +36,7 @@ export class LoginComponent implements OnInit {
         "Dados inválidos", "Erro", { duration: 5000 });
       return;
     }
-    alert(JSON.stringify(this.form.value));
+    const login: Login = this.form.value;
+    alert('Email: ' + login.email + " Senha: " + login.senha);
   }
 }
